0d2db1b4b742117cd44a7d9b999fe7af52c2114f
Ishnu Suresh <ishnu50@gmail.com>
2022-07-26 01:17:21 -0400

    Initial Commit

---
+++ CO-LIFT/CO_LIFTApp.swift
@@ -0,0 +1,17 @@
+//
+//  CO_LIFTApp.swift
+//  CO-LIFT
+//
+//  Created by Ishnu Suresh on 2022-07-26.
+//
+
+import SwiftUI
+
+@main
+struct CO_LIFTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
